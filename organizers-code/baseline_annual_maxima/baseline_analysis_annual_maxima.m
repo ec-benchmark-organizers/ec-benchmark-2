1d28ec73e00d1a327c22456404b8971f1c2d7246
edmackay <43071378+edmackay@users.noreply.github.com>
2021-05-20 14:41:24 +0100

    Add files via upload

--- organizers-code/baseline_annual_maxima/baseline_analysis_annual_maxima.m
+++ organizers-code/baseline_annual_maxima/baseline_analysis_annual_maxima.m
@@ -1,10 +1,11 @@
 clear
 
-% load data
-exercise=2;
+% select case
+exercise=1;
 site=1;
-filename=['Exercise' num2str(exercise) '_Site' num2str(site) '_FIOESM_data.csv'];
 
+% load data
+filename=['Exercise' num2str(exercise) '_Site' num2str(site) '_FIOESM_data.csv'];
 data=dlmread(filename,',',1,0);
 year=data(:,1);
 month=data(:,2);
@@ -29,10 +30,10 @@ N=logspace(0,3,100);
 P=1-1./N;
 x=gumbel_inv(P,mu,sigma);
 
-% calculate return values 
+% calculate return values
 T=[5 50 500];
 RV=gumbel_inv(1-1./T,mu,sigma);
- 
+
 % bootstrap record to get confidence interval
 trials=1000;
 x_boot=zeros(trials,length(P));
@@ -46,24 +47,47 @@ end
 x_boot=sort(x_boot);
 RV_boot=sort(RV_boot);
 
-% create array of return values and 95% confidence bounds
-return_values=zeros(3,3);
-for i=1:3
-    return_values(i,:)=[RV(i), RV_boot(round(trials*0.025),i), RV_boot(round(trials*0.975,i),i)];
+% write return value outputs
+if exercise==1
+    filename=['Ex' num2str(exercise) '_Site' num2str(site) '_ReturnValues_Baseline.csv'];
+    header='Return Period [years],Return Value [m],CI 2.5%% [m],CI 97.5%% [m]\n';
+elseif exercise==2
+    filename=['Ex' num2str(exercise) '_Site' num2str(site) '_Quantiles_Baseline.csv'];
+    header='Exceedance Prob,Quantile [m],CI 2.5%% [m],CI 97.5%% [m]\n';
 end
+fid=fopen(filename,'w');
+fprintf(fid,header);
+fclose(fid);
+RVlow=RV_boot(round(trials*0.025),:);
+RVhigh=RV_boot(round(trials*0.975,i),:);
+dlmwrite(filename,[T(:) RV(:) RVlow(:) RVhigh(:)],'-append');
+
+% write distribution outputs
+if exercise==1
+    filename=['Ex' num2str(exercise) '_Site' num2str(site) '_AnnualDist_Baseline.csv'];
+elseif exercise==2
+    filename=['Ex' num2str(exercise) '_Site' num2str(site) '_F25Dist_Baseline.csv'];
+end
+header='Exceedance Prob,Quantile [m],CI 2.5%% [m],CI 97.5%% [m]\n';
+fid=fopen(filename,'w');
+fprintf(fid,header);
+fclose(fid);
+xlow=x_boot(round(trials*0.025),:);
+xhigh=x_boot(round(trials*0.975),:);
+dlmwrite(filename,[1-P(:) x(:) xlow(:) xhigh(:)],'-append');
 
 % calculate empirical exceedance probabilities
 n=length(Annmax);
 k=1:n;
-Pempirical=(k-0.31)/(n+0.38);
+Pempirical=k/(n+1);
 
 % plot results
 figure
 hold on; box on; grid on
 plot(sort(Annmax),1-Pempirical,'ko')
 plot(x,1-P,'r')
-plot(x_boot(round(trials*0.025),:),1-P,'b--')
-plot(x_boot(round(trials*0.975),:),1-P,'b--')
+plot(xlow,1-P,'b--')
+plot(xhigh,1-P,'b--')
 xlabel('Annual Max Hs [m]')
 ylabel('Exceedance probability')
 title(['Exercise ' num2str(exercise) ', Site ' num2str(site)])
@@ -73,4 +97,3 @@ set(gca,'YMinorTick','off')
 set(gca,'YMinorGrid','off')
 set(gca,'YTick',[1e-2 2e-2 1e-1 2e-1 1])
 set(gca,'YTickLabel',{'1/100','1/50','1/10','1/5','1'})
-
